3d8b3488a2a79dc4ad742f4a17f019a405147d6e
publiminal <publiminal@gmail.com>
2022-04-15 06:48:51 -0400

    added class functionality

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,34 +1,189 @@
 import React from 'react'
 
+// The coordinates of each square of the grid are as follows:
+// (1, 1) (2, 1) (3, 1)
+// (1, 2) (2, 2) (3, 2)
+// (1, 3) (2, 3) (3, 3)
+
+// 2D arrays or matrices are typical structures used to represent grids:
+// [[null, null, null], [null, "B", null], [null, null, null]]
+// [null, null, null, null, "B", null, null, null, null]
+
+
+// Using a "getCoordinates" helper function you could build:
+// const [x, y] = getCoordinates(grid)
+// console.log(`(${x}, ${y})`) // (1, 2)
+
+
+class Square extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div 
+        info={this.props.info} 
+        className={`square${this.props.selected ? ' active' : ''}`}
+      >
+        {this.props.selected}
+      </div>
+    )
+  }
+}
+
+
+
+
 export default class AppClass extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.initBoard = [
+      [1,1,''],[2,1,''],[3,1,''],
+      [1,2,''],[2,2,''],[3,2,''],
+      [1,3,''],[2,3,''],[3,3,'']
+    ]  
+  
+    this.state = {
+      board:[
+        [1,1,''],[2,1,''],[3,1,''],
+        [1,2,''],[2,2,'B'],[3,2,''],
+        [1,3,''],[2,3,''],[3,3,'']
+      ],
+      totalMoves:0, 
+      currentSquare:[[2,2,'B'],4],
+      left:false, 
+      right:false, 
+      up:false,
+      down:false, 
+      message:''
+    }
+
+  }
+
+  /* return the current slice and its idx looking up for the letter B */
+  getCurrentSquare = () => {
+    let coord=[]
+    this.state.board.map((square, idx) => {
+       if(square[2] === 'B') {coord = [square, idx]}
+    })
+    return coord
+  }
+
+  getSquareById = (searchSquare) => {
+    let coord=[]
+    this.state.board.map((square, idx) => {
+       if(square[0] === searchSquare[0] && square[1] === searchSquare[1] && square[2] === searchSquare[2] ) {coord = [square, idx]}
+    })
+    return coord
+  }
+
+
+  setCurrentSquare = (square) => {
+      const newBoard =  JSON.parse(JSON.stringify(this.state.board));
+      // console.log(newBoard)
+      const [ newSquare, newIdx ] = square
+      const [ oldSquare, oldIdx ] = this.getCurrentSquare() 
+      newSquare[2]='B' // update letter at new square
+      newBoard[newIdx] = newSquare
+      newBoard[oldIdx][2] = '' // remove letter at old square
+      // console.log(newBoard)
+      this.setState({
+        currentSquare:square,
+        board:newBoard 
+      })
+  }
+  
+  // )
+
+  squareHandler = (direction) => {
+    console.log('////////////////////////////////////////')
+    console.log('target',direction)
+    const [ square, idx ] = this.getCurrentSquare()
+    console.log('square' , square)
+
+    const currentColumn = square[0]
+    console.log('currentColumn' , currentColumn)
+
+    const currentRow = square[1]
+    console.log('currentRow' , currentRow)
+
+    let newSquare = this.getCurrentSquare() // [[column,row, 'letter'],idx]
+    let msg = ''
+    let isMovable = false
+    if(direction === 'left'){
+        isMovable = currentColumn-1 > 0
+        if(isMovable){
+            newSquare = this.getSquareById([currentColumn-1,currentRow, ''])
+            // this.setCurrentSquare(newSquare)
+          }else{msg = `You can't go left`}
+        }
+
+        if(direction === 'right'){
+          isMovable = currentColumn+1 <= 3
+          if(isMovable){
+              newSquare = this.getSquareById([currentColumn+1,currentRow, ''])
+            }else{msg = `You can't go right`}
+          } 
+
+        if(direction === 'up'){
+          isMovable = currentRow-1 > 0  
+          if(isMovable){
+              newSquare = this.getSquareById([currentColumn,currentRow-1, ''])
+            }else{msg = `You can't go up`}
+          }     
+
+        if(direction === 'down'){
+          isMovable = currentRow+1 <= 3
+          if(isMovable){
+              newSquare = this.getSquareById([currentColumn,currentRow+1, ''])
+            }else{msg = `You can't go down`}
+          } 
+          
+        if(direction === 'reset'){
+          isMovable = false
+          msg = ``
+          this.setState({totalMoves:0})         
+          newSquare = this.getSquareById([2,2, ''])
+          this.setCurrentSquare(newSquare)
+        }   
+        
+        if(isMovable) {
+          this.setCurrentSquare(newSquare)
+          this.setState({totalMoves:this.state.totalMoves+1})
+        }
+        this.setState({message:msg})
+  }
+
+
   render() {
     const { className } = this.props
+    const [square, idx] = this.getCurrentSquare();
+    console.log('square', square)
+    // const moveColumn = square[0]
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({`${square[0]}, ${square[1]}`})</h3>
+          <h3 id="steps">You moved {this.state.totalMoves} times</h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+
+
+        {this.state.board.map( (square, idx) => (
+            <Square key={idx} info={square} selected={square[2]} />
+        ))}
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button disabled={this.state.left} onClick={() => this.squareHandler('left')} id="left">LEFT</button>
+          <button onClick={() => this.squareHandler('up')}  id="up">UP</button>
+          <button onClick={() => this.squareHandler('right')} id="right">RIGHT</button>
+          <button onClick={() => this.squareHandler('down')} id="down">DOWN</button>
+          <button onClick={() => this.squareHandler('reset')} id="reset">reset</button>
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
